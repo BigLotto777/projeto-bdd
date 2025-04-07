f99e8cf1ae5ec21298c7b6d6bbe33e9344fbe53e
Vitor Murgi Beloto <vitor.beloto@dinfor.uniceub>
2025-04-07 09:29:52 -0300

    Edição atualizada

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
- <?php 
- include_once("conexao.php"); 
-$id = isset($_GET['id']) ? $_GET['id']: null;
-if($id){
+<?php
+include_once("conexao.php");
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+if ($id) {
     $sql = "SELECT * FROM aluno WHERE id = :id";
     $stmt = $conexao->prepare($sql);
     $stmt->bindParam(':id', $id);
@@ -9,35 +9,36 @@ if($id){
     $resultItem = $stmt->fetch(PDO::FETCH_OBJ);
 }
 
- $sql = "SELECT * FROM aluno";
- $result = $conexao->query($sql);
+$sql = "SELECT * FROM aluno";
+$result = $conexao->query($sql);
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="inserir.php" method="POST">
-        NOME: <input type="text" name="nome" 
-        value="<?php echo isset ($resultItem) ? $resultItem->nome: ''?>">
+        <input type="hidden" name="id" value="<?php echo isset($resultItem) ? $resultItem->id : '' ?>">
+
+        NOME: <input type="text" name="nome" value="<?php echo isset($resultItem) ? $resultItem->nome : '' ?>">
         <br><br>
-        RA: <input type="number" name="ra"
-        value="<?php echo isset ($resultItem) ? $resultItem->ra: ''?>">
+        RA: <input type="number" name="ra" value="<?php echo isset($resultItem) ? $resultItem->ra : '' ?>">
         <br><br>
-        EMAIL: <input type="text" name="email"
-        value="<?php echo isset ($resultItem) ? $resultItem->email: ''?>">
+        EMAIL: <input type="text" name="email" value="<?php echo isset($resultItem) ? $resultItem->email : '' ?>">
         <br><br>
         <input type="submit" value="Salvar">
         <input type="reset" value="Limpar">
-        <br><br>
+        <br><br> 
     </form>
 
-    <table border = "1" width = "100%">
+    <table border="1" width="100%">
         <tr>
             <th>ID</th>
             <th>NOME</th>
@@ -45,19 +46,20 @@ if($id){
             <th>EMAIL</th>
             <th>AÇÕES</th>
         </tr>
-        <?php while($row = $result->fetch(PDO::FETCH_OBJ)) { ?>
-        <tr>
-            <td><?php echo $row->id ?></td>
-            <td><?php echo $row->nome ?></td>
-            <td><?php echo $row->ra ?></td>
-            <td><?php echo $row->email ?></td>
-            <td>
-                <a href="excluir.php?id=<?php echo $row->id ?>">Excluir</a>
-                <a href="index.php?id=<?php echo $row->id ?>">Editar</a>
-            </td>
-        </tr>
-        
+        <?php while ($row = $result->fetch(PDO::FETCH_OBJ)) { ?>
+            <tr>
+                <td><?php echo $row->id ?></td>
+                <td><?php echo $row->nome ?></td>
+                <td><?php echo $row->ra ?></td>
+                <td><?php echo $row->email ?></td>
+                <td>
+                    <a href="excluir.php?id=<?php echo $row->id ?>">Excluir</a>
+                    <a href="index.php?id=<?php echo $row->id ?>">Editar</a>
+                </td>
+            </tr>
+
         <?php } ?>
     </table>
 </body>
+
 </html>
--- inserir.php
+++ inserir.php
@@ -1,17 +1,30 @@
-<?php 
+<?php
 include_once("conexao.php");
+
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
 $nome = $_REQUEST['nome'];
 $ra = $_REQUEST['ra'];
 $email = $_REQUEST['email'];
 
-$sql = "INSERT INTO aluno (nome, ra, email) 
-       VALUES (:nome, :ra, :email)";
+if ($id) {
+       $sql = "UPDATE aluno SET nome = :nome, ra = :ra, email = :email
+        WHERE id = :id";
 
-$stmt = $conexao->prepare($sql);
+       $stmt = $conexao->prepare($sql);
+       $stmt->bindParam(':id', $id);
+       $mensagem = "registro alterado com sucesso";
+
+} else {
+       $sql = "INSERT INTO aluno (nome, ra, email) 
+       VALUES (:nome, :ra, :email)";
+       $stmt = $conexao->prepare($sql);
+       $mensagem = "registro salvo com sucesso";
+}
 $stmt->bindParam(':nome', $nome);
 $stmt->bindParam(':ra', $ra);
 $stmt->bindParam(':email', $email);
+
 $stmt->execute();
 
-header("Location: index.php");
+header("Location: index.php?mensagem=$mensagem");
 ?>
